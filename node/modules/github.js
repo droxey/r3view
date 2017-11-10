913018806a00f0711312488410362514e9cfd3fc
Dani Roxberry <dani@bitoriented.com>
2017-11-10 04:17:16 -0800

    [wip] github

---
+++ node/modules/github.js
@@ -0,0 +1,29 @@
+const GitHub = require('github-api');
+
+
+module.exports = function(socket, token) {
+    let selectedRepo = {};
+
+    var gh = new GitHub({
+        token: token
+    });
+
+    var me = gh.getUser(); // no user specified defaults to the user for whom credentials were provided
+
+    var userRepos;
+    me.listRepos(function(err, repos) {
+        repoList = repos;
+    });
+
+    var repo = gh.getRepo('droxey', 'instascraper');
+    repo.getRef('heads/master', function(err, sha) {
+        if (err) {
+            throw err;
+        }
+
+        repo.getTree('master', function(err, tree) {
+            socket.broadcast(tree);
+        });
+    });
+};
+
